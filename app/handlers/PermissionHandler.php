f757c8a498176bc186be5d9dac1c6e49ec95b85b
harryxu <harryzhxu@gmail.com>
2013-07-20 20:29:45 +0800

    Add more cpanel permissions.

--- app/handlers/PermissionHandler.php
+++ app/handlers/PermissionHandler.php
@@ -6,8 +6,19 @@ class PermissionHandler
     public function onGetAll($permissions)
     {
         $newPermissions = array_merge($permissions->getArrayCopy(), array(
-            'groups.update' => 'Update group',
-            'groups.create' => 'Create group',
+            'admin.view' => 'Access admin area',
+
+            // users
+            'users.view' => 'View users', 
+            'users.create' => 'Create new user', 
+            'users.update' => 'Update exist user', 
+            'users.delete' => 'Delete user',
+
+            // groups
+            'groups.view' => 'View groups', 
+            'groups.create' => 'Create new group', 
+            'groups.update' => 'Update exist group', 
+            'groups.delete' => 'Delete group',
         ));
         $permissions->exchangeArray($newPermissions);
     }
