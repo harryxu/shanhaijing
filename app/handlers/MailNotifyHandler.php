c9493ede1e71c8fe1a069c124f1250c73799462b
jeff <xtigerj@gmail.com>
2013-07-29 22:18:03 +0800

    Changed to make the limitation variable configurable.

--- app/handlers/MailNotifyHandler.php
+++ app/handlers/MailNotifyHandler.php
@@ -5,7 +5,7 @@ class MailNotifyHandler {
     function onAggregateNotification()
     {
         $lastID = Variable::get('notification_email_cron', 0);
-        $results = DB::select('SELECT id, msg, user_id FROM notifications WHERE id > ? AND type = "post" AND readed = 0 ORDER BY id ASC LIMIT 20', array($lastID));
+        $results = DB::select('SELECT id, msg, user_id FROM notifications WHERE id > ? AND type = "post" AND readed = 0 ORDER BY id ASC LIMIT ?', array($lastID, Variable::get('notification_email_limit', 20)));
         if (!empty($results)) {
             $len = count($results);
             Variable::set('notification_email_cron', $results[$len-1]->id);
